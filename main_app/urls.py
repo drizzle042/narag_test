4e124548dd576d6661403c46f89a9a1f53d63684
Chukwujiobi <chukwujiobicanon@gmail.com>
2023-02-20 14:36:42 +0100

    url mismatch resolved

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from main_app import views
 
 urlpatterns = [
-    path('/', TemplateView.as_view(template_name='main.html'), name='main-user-interface'),
+    path('', TemplateView.as_view(template_name='main.html'), name='main-user-interface'),
     path('auth/', views.SignInView.as_view(), name='gen_auth'),
     path('file-manager/', views.FileManagementView.as_view(), name='file-manager'),
     path('file-reader/', views.FileReaderView.as_view(), name='file-reader'),
